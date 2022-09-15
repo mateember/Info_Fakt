16d6e4a302c7ff00ca0ff2dc4c7ff9fc6a509159
mateember <63301981+mateember@users.noreply.github.com>
2022-09-15 22:16:26 +0200

    Add files via upload

--- Programozás/2013_majus_emelt/valasztasok.py
+++ Programozás/2013_majus_emelt/valasztasok.py
@@ -1,1 +1,18 @@
+print('1. feladat')
+beolvas = open('szavazatok.txt', "r")
+szavazatok = []
+for sor in beolvas:
+    szavazat = sor.strip().split(' ')
+    szavazatok.append([int(szavazat[0]), int(szavazat[1]), szavazat[2], szavazat[3], szavazat[4]])
+
+#print(szavazatok)
+
+print('2.feladat:\nA helyhatósági választáson', int(len(szavazatok), ), 'képviselőjelölt indult.')
+
+print('3. feladat: ')
+be_vnev = input('Adja meg a képviselő vezetéknevét: ')
+be_unev = input('Adja meg a képviselő keresztnevét: ')
+be_nev = be_vnev + ' ' + be_unev
+print(be_nev)
+
 
