d17949399f6b19fd98e08b6bfab133e8c639e644
mateember <63301981+mateember@users.noreply.github.com>
2022-09-16 18:03:55 +0200

    Add files via upload

--- Programozás/2013_majus_emelt/valasztasok.py
+++ Programozás/2013_majus_emelt/valasztasok.py
@@ -13,6 +13,11 @@ print('3. feladat: ')
 be_vnev = input('Adja meg a képviselő vezetéknevét: ')
 be_unev = input('Adja meg a képviselő keresztnevét: ')
 be_nev = be_vnev + ' ' + be_unev
-print(be_nev)
+#print(be_nev)
 
-
+for teszt in szavazatok:
+    if str(teszt[2]) + " " + str(teszt[3]) == be_nev:
+        print('A jelölt', teszt[1], 'szavazatot kapott')
+    else:
+        print('Ilyen nevű képviselőjelölt nem szerepel a nyilvántartásban!')
+        break
