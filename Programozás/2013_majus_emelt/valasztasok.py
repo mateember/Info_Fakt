fd19ec13770bde1612a8f8466a6e300520b62fc2
mateember <63301981+mateember@users.noreply.github.com>
2022-09-22 16:17:31 +0200

    Add files via upload

--- Programozás/2013_majus_emelt/valasztasok.py
+++ Programozás/2013_majus_emelt/valasztasok.py
@@ -27,15 +27,29 @@ if not Letezik:
 print("4.feladat")
 jogosult = 12345
 szavazott = 0
-for sor2 in szavazatok:
-    szavazott = szavazott + int(sor2[1])
+for sor in szavazatok:
+    szavazott += int(sor[1])
 
 arany = (szavazott / jogosult) * 100
 print("A választáson", str(szavazott), "állampolgár, a jogosultak", "%.2f" % arany + "%-a vett részt.\n")
 
 print("5. feladat:")
 
-gyep = 0.0
+szervezetek = [["Gyümülcsevők Pártja", "GYEP"],
+               ["Húsevők Pártja", "HEP"],
+               ["Zöldségevők Pártja", "ZEP"],
+               ["Tejivók Szövetsége", "TISZ"],
+               ["Független jelöltek", "-"]
+               ]
+
+for szervezet in szervezetek:
+    voksok = 0
+    for sor in szavazatok:
+        if szervezet[1] == sor[4]:
+            voksok += sor[1]
+    print(szervezet[0] + ": " + str(round((voksok / szavazott) * 100, 2)) + "%")
+
+'''gyep = 0.0
 hep = 0.0
 tisz = 0.0
 zep = 0.0
@@ -43,39 +57,66 @@ fuggetlen = 0.0
 
 for i in szavazatok:
     if i[4] == '-':
-        fuggetlen = fuggetlen + float(i[1])
+        fuggetlen += float(i[1])
     elif i[4] == 'GYEP':
-        gyep = gyep + float(i[1])
+        gyep += float(i[1])
     elif i[4] == 'HEP':
-        hep = hep + float(i[1])
+        hep += float(i[1])
     elif i[4] == 'TISZ':
-        tisz = tisz + float(i[1])
+        tisz += float(i[1])
     elif i[4] == 'ZEP':
-        zep = zep + float(i[1])
+        zep += float(i[1])
 
-osszszavazat = 0
 
-for i in szavazatok:
-    osszszavazat = osszszavazat + i[1]
-print("Gyümülcsevők Pártja: " + "%.2f" % ((gyep / osszszavazat) * 100) + "%")
-print("Húsevők Pártja: " + "%.2f" % ((hep / osszszavazat) * 100) + "%")
-print("Zöldségevők Pártja: " + "%.2f" % ((zep / osszszavazat) * 100) + "%")
-print("Tejivók Szövetsége: " + "%.2f" % ((tisz / osszszavazat) * 100) + "%")
-print("Független jelöltek: " + "%.2f" % ((fuggetlen / osszszavazat) * 100) + "%")
-
+print("Gyümülcsevők Pártja: " + "%.2f" % ((gyep / szavazott) * 100) + "%")
+print("Húsevők Pártja: " + "%.2f" % ((hep / szavazott) * 100) + "%")
+print("Zöldségevők Pártja: " + "%.2f" % ((zep / szavazott) * 100) + "%")
+print("Tejivók Szövetsége: " + "%.2f" % ((tisz / szavazott) * 100) + "%")
+print("Független jelöltek: " + "%.2f" % ((fuggetlen / szavazott) * 100) + "%")
+'''
 print("\n6. feladat")
 
 legtobb = 0
-legtobbsz = [0]
-for j in szavazatok:
-    if legtobb < j[1]:
-        legtobb = j[1]
-        legtobbsz[0] = j[1]
+legtobbsz = 0
+for sor in szavazatok:
+    if legtobb < sor[1]:
+        legtobb = sor[1]
+        legtobbsz = sor[1]
 
-for i in szavazatok:
-    if legtobbsz[0] == i[1]:
-        print("A legtöbb szavazatot", i[2], i[3], "kapta, ", end="")
-        if i[4] == "-":
-            print("független képviselő.")
+print("A legtöbb szavazatot kapó képviselőjelöltek, (", legtobbsz, ") szavazattal:", sep="")
+
+for sor in szavazatok:
+    if legtobbsz == sor[1]:
+        print(sor[2], sor[3], end="")
+        if sor[4] == "-":
+            print(", független képviselő.")
         else:
-            print("a", i[4], "párt jelöltje")
+            print(", a", sor[4], "párt jelöltje.")
+'''
+for sor in szavazatok:
+    if legtobbsz == sor[1]:
+        print("A legtöbb szavazatot", sor[2], sor[3], "kapta, ", end="")
+        if sor[4] == "-":
+            print("független képviselő", end="")
+        else:
+            print("a", sor[4], "párt jelöltje", end="")
+        print(", (", legtobbsz, ") szavazattal.", sep="")'''
+
+print("7.feladat")
+# rendezzük az adatokat választókerület és szavazato száma szerint szerint
+for i in range(len(szavazatok)):
+    for j in range(len(szavazatok)-i-1):
+        if szavazatok[j][0] > szavazatok[j+1][0]:
+            csere = szavazatok[j]
+            szavazatok[j] = szavazatok[j+1]
+            szavazatok[j+1] = csere
+
+for i in range(len(szavazatok)):
+    for j in range(len(szavazatok)-i-1):
+        if szavazatok[j][0] >= szavazatok[j+1][0] and szavazatok[j][1] < szavazatok[j+1][1]:
+            csere = szavazatok[j]
+            szavazatok[j] = szavazatok[j+1]
+            szavazatok[j+1] = csere
+for sor in szavazatok:
+    print(sor)
+beolvas.close()
